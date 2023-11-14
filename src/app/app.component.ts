cc96624dcbefbca4fee0a162e59837890f3bbf80
Vinicios Neves <vinicios_neves@hotmail.com>
2023-11-14 14:22:09 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+interface Jogo {
+  nome: string | null
+  anoLancamento: string  | null
+  srcImagem: string | null
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'signals';
+  nome = new FormControl('');
+  anoLancamento = new FormControl('2023');
+  srcImagem = new FormControl('');
+
+  jogosSig = signal<Jogo[]>([])
+
+  cadastrar (event: SubmitEvent) {
+    event.preventDefault()
+    this.jogosSig.update(jogos => {
+      jogos.push({
+        nome: this.nome.value,
+        anoLancamento: this.anoLancamento.value,
+        srcImagem: this.srcImagem.value,
+      })
+      return jogos
+    })
+  }
+
 }
